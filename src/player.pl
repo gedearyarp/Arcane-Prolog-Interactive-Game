dcc3b4254b89b47a1a0f6110213bf5f359f8e6d2
Fayza Nadia <74369090+fayzanadia@users.noreply.github.com>
2021-11-27 11:11:33 +0700

    Edit 'farm' and 'ranch'

--- src/player.pl
+++ src/player.pl
@@ -1,8 +1,8 @@
 :- dynamic(player/10).
 :- dynamic(job/1).
 :- dynamic(level/1).
-:- dynamic(farmLevel/1).
-:- dynamic(farmExp/1).
+:- dynamic(farmingLevel/1).
+:- dynamic(farmingExp/1).
 :- dynamic(fishingLevel/1).
 :- dynamic(fishingExp/1).
 :- dynamic(ranchingLevel/1).
@@ -13,12 +13,12 @@
 
 % job('').
 % level(1).
-% farmLevel(1). 
-% farmExp (1).
+% farmingLevel(1). 
+% farmingExp (1).
 % fishingLevel(1). 
 % fishingExp(1).
-% ranchLevel(1).
-% ranchExp(1).    
+% ranchingLevel(1).
+% ranchingExp(1).    
 % exp(1).
 % baseExp(1). 
 % gold(1).
@@ -28,17 +28,17 @@ initPlayer :-   write('In order to play, please choose your role in this world(1
                 write('2. Farmer'), nl,
                 write('3. Rancher'), nl,
                 read(Input), nl, 
-                (Input =:= 1 -> asserta(job('Fisherman')), asserta(level(1)), asserta(farmLevel(1)), asserta(farmExp(56)), asserta(fishingLevel(1)), asserta(fishingExp(76)), asserta(ranchLevel(1)), asserta(ranchExp(56)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
-                Input =:= 2 -> asserta(job('Farmer')), asserta(level(1)), asserta(farmLevel(1)), asserta(farmExp(76)), asserta(fishingLevel(1)), asserta(fishingExp(56)), asserta(ranchLevel(1)), asserta(ranchExp(56)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
-                Input =:= 3 -> asserta(job('Rancher')), asserta(level(1)), asserta(farmLevel(1)), asserta(farmExp(56)), asserta(fishingLevel(1)), asserta(fishingExp(56)), asserta(ranchLevel(1)), asserta(ranchExp(76)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
+                (Input =:= 1 -> asserta(job('Fisherman')), asserta(level(1)), asserta(farmingLevel(1)), asserta(farmingExp(56)), asserta(fishingLevel(1)), asserta(fishingExp(76)), asserta(ranchingLevel(1)), asserta(ranchingExp(56)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
+                Input =:= 2 -> asserta(job('Farmer')), asserta(level(1)), asserta(farmingLevel(1)), asserta(farmingExp(76)), asserta(fishingLevel(1)), asserta(fishingExp(56)), asserta(ranchingLevel(1)), asserta(ranchingExp(56)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
+                Input =:= 3 -> asserta(job('Rancher')), asserta(level(1)), asserta(farmingLevel(1)), asserta(farmingExp(56)), asserta(fishingLevel(1)), asserta(fishingExp(56)), asserta(ranchingLevel(1)), asserta(ranchingExp(76)), asserta(exp(0)), asserta(baseExp(300)), asserta(gold(500));
                 write('Wrong input! Please input the right command'), nl, initPlayer).
 
 initPlayer :-   write('The player has already created, you can\'t initialize again.').
 
 status :-       job(A),
                 level(B),
-                farmLevel(C), 
-                farmExp(D),
+                farmingLevel(C), 
+                farmingExp(D),
                 fishingLevel(E), 
                 fishingExp(F),
                 ranchingLevel(G),
