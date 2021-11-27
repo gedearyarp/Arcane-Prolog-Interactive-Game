380263c68461a3d9057d8d921dffcba9f2473f89
gagaspbahar <gagaspbahar@gmail.com>
2021-11-28 02:07:46 +0700

    Quest done (belom dicek XP)

--- src/quest.pl
+++ src/quest.pl
@@ -1,34 +1,102 @@
-:- dynamic(currentQuest/5).
+:- dynamic(currentQuest/2).
+:- dynamic(fishQuest/1).
+:- dynamic(cropQuest/1).
+:- dynamic(dairyQuest/1).
 
+% inQuest(true).
+
+
+% Crop, Fish, Dairy, Gold, Xp
 
 initQuest :-
-    currentQuest(_,_,_,_,_),nl,
-    write("Anda sedang menjalankan quest. Sarannya sih, selesaiin dulu, daripada keos."),
+    currentQuest(_,_),nl,
+    write('Anda sedang menjalankan quest. Sarannya sih, selesaiin dulu, daripada keos.'),
     nl, !.
 
 initQuest :-
-    \+(currentQuest(_,_,_,_,_)),
-    random(0,10,Crop),
-    random(0,10,Fish),
-    random(0,10,Dairy),
+    \+(currentQuest(_,_)),
+    random(1,10,Crop),
+    random(1,10,Fish),
+    random(1,10,Dairy),
     G is (Crop+Fish+Dairy)*10,
     X is (Crop+Fish+Dairy)*10,
-    asserta(currentQuest(Crop,Fish,Dairy,G,X)).
+    asserta(currentQuest(G,X)),
+    asserta(fishQuest(Fish)),
+    asserta(cropQuest(Crop)),
+    asserta(dairyQuest(Dairy)),
+    write('Anda mendapat quest baru. Kumpulkan '), write(Crop), write(' buah crop, '),
+    write(Fish), write(' ekor ikan, dan '), write(Dairy), write(' buah dairy. Goodluck!'), nl,
+    !.
 
-finishQuest :-
-    gold(OldG),
-    % xp(OldX), blm dibuat
+
+checkQuest :-
     currentQuest(0,0,0,G,X),
+    gold(OldG),
+    xp(OldX),
     NewG is G + OldG,
     NewX is X + OldX,
-    write("Selamat, anda telah menyelesaikan Quest!").
-    % Print variablesnya jg
-    % Lanjut besok lg
+    retract(gold(_)),
+    asserta(gold(NewG)),
+    retract(xp(_)),
+    asserta(xp(NewX)),
+    write('Selamat, anda telah menyelesaikan Quest!'), nl,
+    write('Gold yang didapat: '), write(G), nl,
+    write('Experience yang didapat: '), write(X),nl,
+    !.
 
 
 quest :-
-    \+(currentQuest(_,_,_,_,_)),nl,
-    write("Tidak ada quest yang sedang dijalankan.").
+    \+(inQuest(_)),
+    \+(currentQuest(_,_)),
+    write('Tidak ada quest yang sedang dijalankan.'),
     !.
 
 quest :-
+    currentQuest(_,_),
+    write('Anda sedang menjalankan quest. Sisa aktivitas yang dibutuhkan: '),nl,
+    (fishQuest(Fish) -> write('Fish: '), write(Fish), nl),
+    (cropQuest(Crop) -> write('Crop: '), write(Crop), nl),
+    (dairyQuest(Dairy) -> write('Dairy: '), write(Dairy), nl),
+    !.
+
+quest :- 
+    inQuest(_),
+    \+currentQuest(_,_),
+    write('Anda menemukan sebuah Quest!'),nl,
+    initQuest,
+    !.
+
+decrementFish :-
+    \+(fishQuest(_)),
+    !.
+
+decrementFish :-
+    fishQuest(Fish),
+    X is Fish - 1,
+    retract(fishQuest(_)),
+    asserta(fishQuest(X)),
+    !.
+
+decrementCrop :-
+    \+(fishQuest(_)),
+    !.
+
+decrementCrop :-
+    cropQuest(Crop),
+    X is Crop - 1,
+    retract(cropQuest(_)),
+    asserta(cropQuest(X)),
+    !.
+
+decrementDairy(N) :-
+    N is N,
+    \+(dairyQuest(_)),
+    !.
+
+decrementDairy(N) :-
+    dairyQuest(Dairy),
+    (N > Dairy -> X is 0;
+    Dairy >= N -> X is Dairy-N),
+    retract(dairyQuest(_)),
+    asserta(dairyQuest(X)),
+    !.
