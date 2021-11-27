2f30bd9d8cc14e8c3a718df2715f9988fd18544f
apwic <16520227@mahasiswa.itb.ac.id>
2021-11-27 20:35:35 +0700

    Add exit command

--- src/start.pl
+++ src/start.pl
@@ -48,8 +48,8 @@ start :-
     repeat,
     nl, write('> '),
     read(X),
-    call(X),
-    fail.
+    (X == 'exit' -> !, write('Thank you for wasting such a precious time to play this game :)'), nl;
+    call(X), fail).
 
 start :-
     write('The game has already started. Use \'help.\' to look at available commands!').
